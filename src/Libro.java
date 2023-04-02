31eb7eb7cc930de315f5a91a5a17ba89f25682cc
aramirezram <aramirezram@unal.edu.co>
2023-04-02 18:29:41 -0500

    Se crea src y Libro.java

---
+++ src/Libro.java
@@ -0,0 +1,49 @@
+
+public class Libro{
+	private String titulo,autor,descripcion,isbn;
+	private int precio;
+	static int numlibros;
+	Libro(String titulo,String autor,String isbn,int precio){
+		this.titulo=titulo;
+		this.autor=autor;
+		this.isbn=isbn;
+		this.precio=precio;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public int getNumlibros() {
+		return numlibros;
+	}
+	public void setNumlibros(int numlibros) {
+		this.numlibros = numlibros;
+	}
+
+}
